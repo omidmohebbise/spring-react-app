b5d04cac49aa0d2330c15ff1439192695b8c39c6
omid mohebbi <omidmohebbise@gmail.com>
2024-01-16 15:32:59 +0100

    initialize the project

---
+++ src/main/java/org/omidmohebbise/graphql/pringreactjsgraphql/PringReactjsGraphqlApplication.java
@@ -0,0 +1,13 @@
+package org.omidmohebbise.graphql.pringreactjsgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringReactjsGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PringReactjsGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/omidmohebbise/graphql/pringreactjsgraphql/PringReactjsGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omidmohebbise.graphql.pringreactjsgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PringReactjsGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
